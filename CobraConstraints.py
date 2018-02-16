ea3a981d5b60a55cefbf643674e904ef4cf4d53d
Dimitri Coukos <dimitri.coukos@epfl.ch>
2018-02-16 17:41:23 -0500

    find max feasible xi, write osmo figures to files instead of showing

--- CobraConstraints.py
+++ CobraConstraints.py
@@ -1,8 +1,11 @@
+import os
+import sys
 import json
 import pandas
 import numpy
 import cobra
-from matplotlib.pyplot import scatter, draw, show
+import matplotlib
+from matplotlib.pyplot import figure
 from optlang.symbolics import Zero
 from cobra.exceptions import Infeasible
 from progress.bar import Bar
@@ -13,10 +16,44 @@ from progress.bar import Bar
 def main():
     k1_model = cobra.io.read_sbml_model('iCHOv1_K1_final.xml')
     k1_updates = openJson('JSONs/k1_updates.json')
-    population_dynamics(k1_model, k1_updates)
+    population_osmolarities(k1_model, k1_updates)
 
 
-def run_fba(model, updates):
+def find_max_xi(model, updates, tolerance=1):
+    assert tolerance > 0
+    min_xi = 0.
+    max_xi = 1000.
+    max_undefined = True
+    print("finding an infeasible xi", end='')
+    while max_undefined:
+        model = cobra.io.read_sbml_model('iCHOv1_K1_final.xml')
+        updates = openJson('JSONs/k1_updates.json')
+        print('.', end='')
+        try:
+            run_fba(model, updates, max_xi)
+            max_xi *= 2
+        except Infeasible:
+            max_undefined = False
+    print("\nfinding max xi", end='')
+    while max_xi - min_xi > tolerance:
+        model = cobra.io.read_sbml_model('iCHOv1_K1_final.xml')
+        updates = openJson('JSONs/k1_updates.json')
+        assert min_xi <= max_xi
+        avg_xi = (max_xi + min_xi)/2
+        print('min_xi=', min_xi, ', max_xi=', max_xi, ', avg_xi=', avg_xi, flush=True)
+        try:
+            run_fba(model, updates, avg_xi)
+            min_xi = avg_xi
+        except Infeasible:
+            print('Caught exception', flush=True)
+            max_xi = avg_xi
+    print('Returning ' + str(min_xi))
+    return min_xi
+
+
+def run_fba(model, updates, xi):
+    enzyme_mass = float(sys.argv[1])
+    constrain_uptakes(model, xi)
     coef_forward = {}
     coef_backward = {}
     release_bounds(model)
@@ -24,32 +61,62 @@ def run_fba(model, updates):
     # TODO: solution used before it is defined? I have added a first FBA to
     #           estimate the enzyme mass.
     # enzyme_mass = get_enzyme_mass(sol, coef_forward, coef_backward)
-    flux_constraint(model, coef_forward, coef_backward, 0.055)
+    flux_constraint(model, coef_forward, coef_backward, enzyme_mass)
     get_coefficients(updates, coef_forward, coef_backward)
     return fba_and_min_enzyme(model, coef_forward, coef_backward)
 
 
-def population_dynamics(model, updates, min_xi=0, max_xi=1200):
+def population_osmolarities(model, updates, min_xi=0):
     # 1st infeasible value = 1202.9
-    slices = max_xi*2
+    # max_xi = 1200/3
+    enzyme_mass = sys.argv[1]
+    max_xi = find_max_xi(model, updates)
+    # max_xi = 1200
+    matplotlib.use('Agg')
+    slices = (max_xi - min_xi)
     xis = []
     osmolarities = []
+    exchanged = []
     medium_osmolarity = 280 * 0  # mM/L (estimate)
     bar = Bar('Calculating osmolarities: ', max=slices)
     for xi in numpy.linspace(min_xi, max_xi, slices):
         bar.next()
         try:
-            constrain_uptakes(model, xi)
-            sol = run_fba(model, updates)
-            osmo = osmolarity(model, sol)
+            sol = run_fba(model, updates, xi)
+            osmo = osmolarity(model, sol, xi)
+            # CHANGED: now a dicitonary with many metabolites.
             xis.append(xi)
-            osmolarities.append(medium_osmolarity - osmo*xi)
+            to_append = {}
+            for mol in osmo:
+                if mol not in exchanged:
+                    exchanged.append(mol)
+                if mol == 'total':
+                    to_append[mol] = medium_osmolarity + osmo[mol]*xi
+                else:
+                    to_append[mol] = osmo[mol]*xi
+            osmolarities.append(to_append)
         except Infeasible:
-            print('xi value: ' + str(xi))
-            raise
-    scatter(xis, osmolarities)
-    draw()
-    show()
+            print('xi value: ' + str(xi) + 'infeasible')
+            break
+
+    bar = Bar('Drawing plots: ', max=len(exchanged))
+    for mol in exchanged:
+        bar.next()
+        ex_over_ss = []
+        for exchanges in osmolarities:
+            if mol not in exchanges:
+                exchanges[mol] = 0
+            ex_over_ss.append(exchanges[mol])
+
+        fig = figure()
+        ax = fig.add_subplot(111)
+        ax.set_title(mol + r' vs. $\xi$')
+        ax.set_ylabel(mol)
+        ax.set_xlabel(r'$\xi$')
+        ax.scatter(xis, ex_over_ss)
+        if not os.path.exists('Figures_' + enzyme_mass):
+            os.mkdir('Figures_' + enzyme_mass)
+        fig.savefig('Figures_' + enzyme_mass + '/' + mol)
 
 
 def openJson(path):
@@ -167,41 +234,50 @@ def get_enzyme_mass(solution, coef_forward, coef_backward):
     return enzyme_mass
 
 
-def exchanges_secretion(cobra_model, solution):
-    "Subset of exchanges that can secrete."
+def exchanges_consumption(cobra_model, solution):
+    "Subset of exchanges that consume."
     import_list = [rxn for rxn in cobra_model.exchanges if
-                   not rxn.products and rxn.upper_bound > 0 or
-                   not rxn.reactants and rxn.lower_bound < 0]
+                   not rxn.products and solution.fluxes[rxn.id] < 0 or
+                   not rxn.reactants and solution.fluxes[rxn.id] > 0]
     imports = {}
     for imp in import_list:
         imports[imp.id] = solution.fluxes[imp.id]
     return imports
 
 
-def exchanges_consumption(cobra_model, solution):
-    "Subset of exchanges that can consume."
+def exchanges_secretion(cobra_model, solution):
+    "Subset of exchanges that secrete"
     export_list = [rxn for rxn in cobra_model.exchanges if
-                   not rxn.products and rxn.lower_bound < 0 or
-                   not rxn.reactants and rxn.upper_bound > 0]
+                   not rxn.products and solution.fluxes[rxn.id] > 0 or
+                   not rxn.reactants and solution.fluxes[rxn.id] < 0]
     exports = {}
     for exp in export_list:
         exports[exp.id] = solution.fluxes[exp.id]
     return exports
 
 
-def osmolarity(model, solution):
-    osmo = float(0)
+def osmolarity(model, solution, xi):
+    '''
+    'Osmolarity' for one steady state.
+
+    '''
+    osmo = {}
+    osmo['total'] = float(0)
     permeable = ['o2_e', 'co2_e', 'h2o_e']
     imports = exchanges_consumption(model, solution)
     exports = exchanges_secretion(model, solution)
+    # TODO: rewrite plotting function to work with dictionaries.
     for mol in imports:
-        if mol not in permeable:
-            osmo += imports[mol]
+        if mol not in permeable and imports[mol] < 0:
+            osmo['total'] += imports[mol]
+            osmo[mol] = imports[mol]
     for mol in exports:
-        if mol not in permeable:
-            osmo += exports[mol]
+        if mol not in permeable and exports[mol] > 0:
+            osmo['total'] += exports[mol]
+            osmo[mol] = exports[mol]
     return osmo
 
 
 if __name__ == '__main__':
+    assert len(sys.argv) > 1
     main()
