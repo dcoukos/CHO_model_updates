5642c17511d6de179235cde360e096a883dd0e08
Dimitri Coukos <dimitri.coukos@epfl.ch>
2018-03-14 20:25:19 +0100

    Get spectra # from excel and match to Uniprot ID

---
+++ Proteomics.py
@@ -0,0 +1,20 @@
+import pandas
+
+
+def main():
+    data = pandas.read_excel('Data/NIHMS413369-supplement-1_si_001.xls',
+                             sheet_name='Supplementary Table 1',
+                             usecols=[4, 9])
+    names = list(data.get(
+                'SwissProt Annotation/Homology with Highest Percentage'))
+    spectra = list(data.get('Number of Identified Spectra'))
+    spectra_dict = {}
+    for index, string in enumerate(names):
+        if str(string) == 'nan':
+            continue
+        ID = string.split(' ')[0]
+        spectra_dict[ID] = int(spectra[index])
+
+
+if __name__ == '__main__':
+    main()
